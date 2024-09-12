826c73942ef679aebd3f5a52c62b7a2ae7f511d5
nmogoko <+>
2024-09-12 21:59:36 +0100

    completed lab

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Mr Chocolate", "10", "M", "White", "cold", "1");
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Rowdy", "10", "M", "Black", "cold", "1");
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Tabitha", "3", "F", "White and Black", "cold", "0");
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Sergeant Brown", "10", "F", "White", "cold", "0");
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Melissa", "6", "M", "White", "cold", "1");
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Grinch", "10", "M", "White", "cold", "1");
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Wendy", "10", "M", "White", "cold", "0");
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Null", "7", "F", "White", "cold", "1");
